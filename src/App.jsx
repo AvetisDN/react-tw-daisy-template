7421167cba1b25015b7c4a7b8075ae7614200826
AvetisDN <avetisdn@gmail.com>
2022-08-21 11:22:12 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./layouts/Layout";
+
+const App = () => {
+  return (
+    <Layout>
+      <div className="text-4xl">App</div>
+    </Layout>
+  );
+};
+
+export default App;
